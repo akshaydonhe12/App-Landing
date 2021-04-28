171d1b1754e79b4dcdcfe65c07a2f738da8b2760
akshaydonhe12 <akshaydonhe02@gmail.com>
2021-04-28 13:33:24 +0530

    video-popup

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+
+
+$(document).ready(function(){
+    
+    /*-----------Navbar ------------- */
+
+    $(window).on("scroll",function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+
+    /*-----------Video Popup ------------- */
+    const videoSrc = $("#player-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click",function(){
+       if($(".video-popup").hasClass("open")){
+        $(".video-popup").removeClass("open");
+        $("#player-1").attr("src","");
+       }
+       else{
+        $(".video-popup").addClass("open");
+        if($("#player-1").attr("src") == ''){
+            $("#player-1").attr("src",videoSrc);
+        }
+       }
+    });
+});
