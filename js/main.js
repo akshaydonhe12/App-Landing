f93aff756ae4453f9a848d4f6b77deb0b668c40e
akshaydonhe12 <akshaydonhe02@gmail.com>
2021-04-29 11:48:54 +0530

    icon-hover

--- js/main.js
+++ js/main.js
@@ -28,4 +28,27 @@ $(document).ready(function(){
         }
        }
     });
+
+    /*-----------Owl-carousel ------------- */
+
+    $('.features-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+        
+            },
+            600:{
+                items:2,
+               
+            },
+            1000:{
+                items:3,
+                
+            }
+        }
+    });
+
 });
