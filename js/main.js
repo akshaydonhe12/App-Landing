a72431620c35260009ac86c58b43a8ee0741de7a
akshaydonhe12 <akshaydonhe02@gmail.com>
2021-04-29 12:15:26 +0530

    features-section-complete

--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
     $('.features-carousel').owlCarousel({
         loop:true,
         margin:0,
+        autoplay:true,
         responsiveClass:true,
         responsive:{
             0:{
