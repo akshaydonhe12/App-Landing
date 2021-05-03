e14263f057701f983c6a26f18540dc31701f98f8
akshaydonhe12 <akshaydonhe02@gmail.com>
2021-05-03 12:28:58 +0530

    Team-section

--- js/main.js
+++ js/main.js
@@ -100,4 +100,30 @@ $(document).ready(function(){
         }
     });
 
+
+
+      
+       /*-----------team-carousel ------------- */
+
+       $('.team-carosual').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+        
+            },
+            600:{
+                items:2,
+               
+            },
+            1000:{
+                items:3,
+                
+            }
+        }
+    });
+
 });
