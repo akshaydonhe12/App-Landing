961783f1a1d08ed78486a3343ffbea4b98052514
akshaydonhe12 <akshaydonhe02@gmail.com>
2021-05-03 12:03:31 +0530

    testimonial-carosual

--- js/main.js
+++ js/main.js
@@ -75,4 +75,29 @@ $(document).ready(function(){
         }
     });
 
+
+    
+       /*-----------testiomial-carousel ------------- */
+
+       $('.testimonial-carousal').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+        
+            },
+            600:{
+                items:2,
+               
+            },
+            1000:{
+                items:3,
+                
+            }
+        }
+    });
+
 });
